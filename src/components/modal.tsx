b0234b794157270222f9b65d94fba34a71fe542f
yskim <yskim@yskimui-MacBookAir.local>
2024-09-05 17:59:22 +0900

    모달 COLORS -> theme 으로 변경

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,8 +1,8 @@
-'use client';
-import { Modal as CustomModal } from '@mui/material';
-import Image from 'next/image';
-import { COLORS } from '@/styles/common';
-import styled from '@emotion/styled';
+"use client";
+import { Modal as CustomModal } from "@mui/material";
+import Image from "next/image";
+import { COLORS } from "@/styles/common";
+import styled from "@emotion/styled";
 
 interface Props {
   open: boolean;
@@ -15,14 +15,25 @@ interface Props {
   children: React.ReactNode;
 }
 
-export default function Modal({ open, onClose, style, hideBackdrop = false, isClose = true, closeSize = 24, closeStyle, children }: Props) {
+export default function Modal({
+  open,
+  onClose,
+  style,
+  hideBackdrop = false,
+  isClose = true,
+  closeSize = 24,
+  closeStyle,
+  children,
+}: Props) {
   return (
     <CustomModal open={open} onClose={onClose} hideBackdrop={hideBackdrop}>
       <EContainer style={style}>
         {isClose && (
           <Image
             className="close"
-            src={'https://image.thetak.net/asset/product/images/x_close_gray_2.svg'}
+            src={
+              "https://image.thetak.net/asset/product/images/x_close_gray_2.svg"
+            }
             alt="close"
             width={closeSize}
             height={closeSize}
@@ -38,13 +49,13 @@ export default function Modal({ open, onClose, style, hideBackdrop = false, isCl
 
 const EContainer = styled.div<{ style?: React.CSSProperties }>`
   position: absolute;
-  top: ${({ style }) => (style?.top ? style.top : '50%')};
+  top: ${({ style }) => (style?.top ? style.top : "50%")};
   left: 50%;
   transform: translate(-50%, -50%);
   padding: 30px;
-  background-color: ${COLORS.grayScale.white};
+  background-color: ${({ theme }) => theme.colors.grayScale.white};
   border-radius: 8px;
-  border: 1px solid ${COLORS.grayScale.gray3};
+  border: 1px solid ${({ theme }) => theme.colors.grayScale.gray3};
 
   :focus {
     outline: none;
